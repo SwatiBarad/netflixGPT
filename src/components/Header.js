ec87d906d2629ae80d294da1ebddeebb45badbd1
SwatiBarad <baradswati2001@gmail.com>
2025-06-09 12:03:21 +0530

    loginpage/signIn/signUp

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="absolute top-0 left-0 z-10">
       <img
-        className="ml-24 filter  mt-6 w-60"
+        className="filter contrast-[200%] brightness-[150%] ml-10 mt-4 w-60"
         src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
         alt="logo"
       />
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
+import { isDisabled } from "@testing-library/user-event/dist/utils";
 
 const Login = () => {
+  const [signIn, setSignIn] = useState(true);
+
+  const toggleSignIn = () => {
+    setSignIn(!signIn);
+  };
   return (
-    <div className="relative h-screen w-full overflow-hidden">
+    <div className="absolute h-screen w-full overflow-x-hidden">
       <Header />
-      <div>
+      <div className="">
         <img
-          className="bg-no-repeat brightness-50 w-full h-screen scale-125"
+          className=" brightness-50 w-full h-screen scale-125 relative "
           src="https://assets.nflxext.com/ffe/siteui/vlv3/6863f6e8-d419-414d-b5b9-7ef657e67ce4/web/IN-en-20250602-TRIFECTA-perspective_27a3fdfa-126f-4148-b153-55d60b51be6a_large.jpg"
           alt="background"
         />
       </div>
+      <form className="w-[32%] rounded-lg p-10 bg-black bg-opacity-60  absolute top-[60%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 m-auto ">
+        <h1 className="text-white font-bold text-3xl my-5">
+          {signIn ? "Sign In" : "Sign Up"}
+        </h1>
+        {!signIn && (
+          <input
+            onChange={signIn ? isDisabled : !isDisabled}
+            type="text"
+            placeholder="User name"
+            className="p-4 my-4 w-full bg-gray-700 rounded-md"
+          />
+        )}
+        <input
+          type="text"
+          placeholder="Email or mobile number"
+          className="p-4 my-4 w-full bg-gray-700 rounded-md"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 w-full bg-gray-700 rounded-md"
+        />
+        <button className="w-full text-white bg-red-800 filter contrast-[150%] brightness-[130%] rounded-md h-10 hover:bg-red-900 transition-colors  my-4">
+          {signIn ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="text-white text-center my-4 ">OR</p>
+        <button className="w-full text-white bg-gray-900  rounded-md h-10 hover:bg-gray-800 transition-colors">
+          Use a sign-in code
+        </button>
+        <p className="text-white my-6">
+          {signIn ? "New to Netflix?" : "Already registered?"}
+
+          <span
+            className="cursor-pointer font-extrabold"
+            onClick={() => {
+              toggleSignIn();
+            }}
+          >
+            {signIn ? " Sign up now." : "Let's sign in"}
+          </span>
+        </p>
+        <p className="text-gray-400 text-sm">
+          This page is protected by Google reCAPTCHA to ensure you're not a bot.
+        </p>
+      </form>
     </div>
   );
 };
